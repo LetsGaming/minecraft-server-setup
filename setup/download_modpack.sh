5f77cee6c1c32f37fd052cb8980a6d92ed8cd7e7
LetsGaming <bergmanncrafter@gmail.com>
2025-04-09 18:30:51 +0200

    Fix path in download_modpack.sh to correctly reference the modpack download script

--- setup/download_modpack.sh
+++ setup/download_modpack.sh
@@ -5,4 +5,4 @@ set -e
 
 # Run the modpack download script
 npm i --no-package-lock --omit=dev
-node "$(dirname "${BASH_SOURCE[0]}")/setup/download/download_modpack.js"
+node "$(dirname "${BASH_SOURCE[0]}")/download/download_modpack.js"
