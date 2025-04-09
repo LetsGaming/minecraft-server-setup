f28c6bdec180510b8cd49f4d2924056c0ae43764
LetsGaming <bergmanncrafter@gmail.com>
2025-04-09 19:21:18 +0200

    Fix path for server-pack.zip removal in main.sh to ensure correct file deletion

--- main.sh
+++ main.sh
@@ -13,6 +13,6 @@ node "$SCRIPT_DIR/setup/create_directories.js"
 node "$SCRIPT_DIR/setup/unpack_modpack.js"
 node "$SCRIPT_DIR/setup/copy_scripts.js"
 
-sudo rm -rf "$MAIN_DIR/server-pack.zip"
+sudo rm -rf "$SCRIPT_DIR/server-pack.zip"
 
 echo "Setup completed successfully."
