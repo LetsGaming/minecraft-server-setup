cdf8c460dbe20aefaee76a5954a544822f4d34af
LetsGaming <bergmanncrafter@gmail.com>
2025-05-03 21:29:09 +0200

    Refactor setup script to ensure repository is cloned only if it doesn't exist and update paths for configuration and management scripts

--- setup/interface/setup_interface.sh
+++ setup/interface/setup_interface.sh
@@ -5,20 +5,23 @@ export INTERFACE_SETUP_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 REPO_URL="https://github.com/LetsGaming/minecraft-server-manager.git"
 
 # --- Clone Repository ---
-git clone "$REPO_URL"
-cd minecraft-server-manager
+if [ ! -d "$INTERFACE_SETUP_SCRIPT_DIR/minecraft-server-manager" ]; then
+  git clone "$REPO_URL" "$INTERFACE_SETUP_SCRIPT_DIR/minecraft-server-manager"
+fi
+
+cd "$INTERFACE_SETUP_SCRIPT_DIR/minecraft-server-manager"
 
 # -- Install Dependencies --
 npm install
-cd ..
+cd  "$INTERFACE_SETUP_SCRIPT_DIR"
 
 # -- Setup Configuration --
-node variables/set_config.js
+node "$INTERFACE_SETUP_SCRIPT_DIR/variables/set_config.js"
 
-node management/move_interface.js
-node management/create_service.js
+node "$INTERFACE_SETUP_SCRIPT_DIR/management/move_interface.js"
+node "$INTERFACE_SETUP_SCRIPT_DIR/management/create_service.js"
 
 # -- Start Interface --
-node management/start_interface.js
+node "$INTERFACE_SETUP_SCRIPT_DIR/management/start_interface.js"
 
 echo "[INFO] Interface setup complete."
